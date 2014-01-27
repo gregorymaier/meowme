23924c6ddd96fe08cddef2bcb5f3b8d66690c691
gregorymaier <greg@Gregorys-MacBook-Air.local>
2014-01-26 23:09:40 -0800

    Removed .class files. Updated combine layout.

--- MeowMe/gen/com/example/meowme/R.java
+++ MeowMe/gen/com/example/meowme/R.java
@@ -24,10 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int croppedImageView=0x7f080001;
-        public static final int imageView=0x7f080000;
-        public static final int what_todo=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int combinedImageView=0x7f080000;
+        public static final int croppedImageView=0x7f080003;
+        public static final int imageView=0x7f080002;
+        public static final int publishButton=0x7f080001;
+        public static final int what_todo=0x7f080004;
     }
     public static final class layout {
         public static final int activity_combine=0x7f030000;
