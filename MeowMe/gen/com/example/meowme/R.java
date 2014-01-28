c6010e789c09c84e40ccc7cca6b19b5a3871dd5f
Alex Cruttenden <crutt101@mail.chapman.edu>
2014-01-28 01:25:42 -0800

    homepage ui

--- MeowMe/gen/com/example/meowme/R.java
+++ MeowMe/gen/com/example/meowme/R.java
@@ -23,12 +23,14 @@ public final class R {
     public static final class drawable {
         public static final int cat_tie=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int meowme_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int combinedImageView=0x7f080000;
         public static final int croppedImageView=0x7f080003;
         public static final int imageView=0x7f080002;
+        public static final int imageView1=0x7f080005;
         public static final int publishButton=0x7f080001;
         public static final int what_todo=0x7f080004;
     }
@@ -86,5 +88,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /** <item name="android:fontFamily">assets/fonts/Walkway UltraBold.ttf</item> 
+         */
+        public static final int MeowMeBtn=0x7f060002;
     }
 }
