cce00a4361bc83712e514692e0c6c520b1de47b1
gregorymaier <greg@Gregorys-MacBook-Air.local>
2014-01-28 19:26:48 -0800

    Updates...

--- MeowMe/gen/com/example/meowme/R.java
+++ MeowMe/gen/com/example/meowme/R.java
@@ -21,18 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int cat_tie=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int meowme_logo=0x7f020002;
+        public static final int cat2_template_720w=0x7f020000;
+        public static final int cat_tie=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int meowme_logo=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080005;
         public static final int combinedImageView=0x7f080000;
-        public static final int croppedImageView=0x7f080003;
         public static final int imageView=0x7f080002;
-        public static final int imageView1=0x7f080005;
+        public static final int imageView1=0x7f080004;
         public static final int publishButton=0x7f080001;
-        public static final int what_todo=0x7f080004;
+        public static final int what_todo=0x7f080003;
     }
     public static final class layout {
         public static final int activity_combine=0x7f030000;
